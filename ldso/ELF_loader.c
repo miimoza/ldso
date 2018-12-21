4c89d11b3c24ef2a862350812f72a61fe071aa53
Antoine Coulon <antoine.coulon@epita.fr>
2018-12-21 15:23:30 +0100

    [dso laoder] try to mmap fix for segment

--- ldso/ELF_loader.c
+++ ldso/ELF_loader.c
@@ -66,7 +66,7 @@ struct ELF *elf_loader(char *pathname, void *addr)
 	return my_elf;
 }
 
-static void set_perm(void *my_dso, void *address, size_t len, int flags, int fd, size_t offset)
+static void set_perm(void *my_dso, ElfW(Addr) address, size_t len, int flags, int fd, size_t offset)
 {
 	int prot = PROT_NONE;
 	if (flags & PF_R)
@@ -76,8 +76,8 @@ static void set_perm(void *my_dso, void *address, size_t len, int flags, int fd,
 	if (flags & PF_X)
 		prot += PROT_EXEC;
 
-	printf("mmap fix:%d\n", mmap((char *) my_dso + (size_t) address, len,
-		prot, MAP_FIXED, fd, offset));
+	printf("mmap fix:%lx\n", mmap((char *) my_dso + address, len,
+		prot, MAP_FIXED | MAP_PRIVATE, fd, offset));
 }
 
 void *dso_loader(struct ELF *my_elf, char *pathname)
@@ -87,17 +87,17 @@ void *dso_loader(struct ELF *my_elf, char *pathname)
 	int fd = open(pathname, O_RDONLY);
 	struct stat stat_buffer;
 	stat(pathname, &stat_buffer);
-	void *my_dso = mmap(0, stat_buffer.st_size, PROT_READ, MAP_ANONYMOUS, -1, 0);
-
+	void *my_dso = mmap(0, stat_buffer.st_size, PROT_READ, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+	printf("my_dso:%lx\n", my_dso);
 
 	ElfW(Phdr) *my_phdr = my_elf->phdr;
     for (int i = 0; i < my_elf->ehdr->e_phnum; i++)
     {
         if (my_phdr->p_type == PT_LOAD)
 		{
-			printf("LOAD SEGMENT.. [vaddr:%016lx memsz:%016lx flags:%d]\n",
-				my_phdr->p_vaddr, my_phdr->p_memsz, my_phdr->p_flags);
-			set_perm(my_dso, (void *) my_phdr->p_vaddr, my_phdr->p_filesz,
+			printf("LOAD SEGMENT.. [vaddr:%016lx offset:%016lx filesz:%016lx flags:%d]\n",
+				my_phdr->p_vaddr, my_phdr->p_offset, my_phdr->p_filesz, my_phdr->p_flags);
+			set_perm(my_dso, my_phdr->p_vaddr, my_phdr->p_filesz,
 				my_phdr->p_flags, fd, my_phdr->p_offset);
 		}
 
